10d424aea7769ee1985a7727aea0901228edba0e
MasterJE <jose.enrique.aquino51@gmail>
2024-01-23 23:01:29 +0800

    APPLIED_VITE_FIX_BUTTONS_UPDATE

--- team-c-react/src/pages/course/course1_hprog.jsx
+++ team-c-react/src/pages/course/course1_hprog.jsx
@@ -15,13 +15,15 @@ function CourseHprog1(){
   switch (pathname) {
  
     case '/course1_hprog':
-      layoutTitle = 'CHAPTER 1';
+      layoutTitle = 'CHAPTER 1'; 
       subTitle = 'Introduction to HTML Programming';
+      descText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
       break;
 
     default:
       layoutTitle = '-NO TITLE-';
       subTitle = '-NO SUBTITLE-';
+      descText = '-NO DESCTEXT-';
       break;
   }
 
@@ -50,55 +52,31 @@ function CourseHprog1(){
                     borderColor: "#0e3b03"
                   }}
                 >
-                  <p className="lh-base">
-                  The "HTML Programming" course provides a comprehensive exploration of Hypertext Markup Language (HTML), which stands as the standard language for creating web pages. In this course, participants delve into the fundamental aspects of HTML, understanding how to structure web content using HTML tags. The curriculum covers key topics such as document structure, text formatting, hyperlinks, images, forms, and multimedia integration. Through hands-on exercises and real-world examples, participants gain practical skills in creating well-structured and visually appealing web pages. This course serves as an essential foundation for anyone aspiring to become proficient in web development, ensuring compatibility and accessibility across various browsers.
-                  </p>
+                  <p className="lh-base">{descText}</p>
                 </div>
-              </div>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end" style={{margin: '10px'}}>
+                <button class="btn btn-primary" 
+                data-bs-toggle="modal" 
+                data-bs-target="#mainId" 
+                className="btn" 
+                type="button"
+                style={{backgroundColor: '#D9FFCF',
+                borderColor: "0E3B03",
+                boxShadow: '0 2px 5px 0 rgb(0 0 0 / 25%), 0 5px 5px 0 rgb(0 0 0 / 30%)'
+                }}
+                >Go to quiz</button>
 
-              {/*BUTTON TO STUDY TOPIC*/}
-              <div className="col-6 d-flex justify-content-center">
-                <button
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                  className="btn"
-                  style={{
-                    color: "#0e3b03",
-                    backgroundColor: "#D9FFCF",
-                    textAlign: "center",
-                    overflow: "visible",
-                    border: "2px solid",
-                    borderColor: "#0e3b03",
-                    align: "center"
-                 
-                  }}
-                >
-                  <img src="/pics/study.png" alt="study" className="zoom"
-                   
-                  />
-                </button>
-              </div>
-              {/*BUTTON FOR QUIZ*/}
-              <div className="col-6 d-flex justify-content-center">
-              <button
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                  className="btn"
-                  style={{
-                    color: "#0e3b03",
-                    backgroundColor: "#D9FFCF",
-                    textAlign: "center",
-                    overflow: "visible",
-                    border: "2px solid",
-                    borderColor: "#0e3b03",
-                    align: "center"
-                   
-                  }}
-                >
-                  <img src="/pics/quizbtn.png" alt="study" className="zoom"
-                 
-                  />
-                </button>
+                <button class="btn btn-primary" 
+                data-bs-toggle="modal" 
+                data-bs-target="#subId" 
+                className="btn" 
+                type="button"
+                style={{backgroundColor: '#0E3B03', 
+                color: 'white',
+                boxShadow: '0 2px 5px 0 rgb(0 0 0 / 25%), 0 5px 5px 0 rgb(0 0 0 / 30%)'
+                }}
+                >Download</button>
+               </div>
               </div>
             </div>
           </div>
@@ -108,7 +86,7 @@ function CourseHprog1(){
       {/* End of Topic Container */}
       {/* End of Main Layout */}
     
-          <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+      <div className="modal fade modalMain" id="mainId" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
       <div className="modal-dialog">
         <div className="modal-content" style={{ backgroundColor: '#D9FFCF' }}>
           <div className="modal-header">
@@ -126,10 +104,27 @@ function CourseHprog1(){
         </div>
       </div>
     </div>
+
+    <div className="modal fade modalSub" id="subId" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+      <div className="modal-dialog">
+        <div className="modal-content" style={{ backgroundColor: '#D9FFCF' }}>
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">Take the quiz?</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body text-center">
+          <p>You will be downloading a resource file.</p><p>Do you wish to Proceed?</p>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" style={{backgroundColor: '#0e3b03', color: '#ffffff', borderRadius: '20px', fontSize: '15px', width: '100px'}}>Proceed</button>
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" style={{backgroundColor: '#0e3b03', color: '#ffffff', borderRadius: '20px', fontSize: '15px', width: '100px' }}>Cancel</button>
+            
+          </div>
+        </div>
+      </div>
+    </div>
     
-          {/* Start of JS for Main Layout */}
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-+qU78ihVd8FhDInHkhNOl3p5nhtvZq+YP1Jv60hyVgvt/aGzfODJzL2mHTi/xIsN" crossorigin="anonymous"></script>
-          {/* End of JS for Main Layout */}
+
         </Fragment>
       );
     }
--- team-c-react/src/pages/course/course3_svn.jsx
+++ team-c-react/src/pages/course/course3_svn.jsx
@@ -54,51 +54,29 @@ function CourseSvn3(){
                 >
                   <p className="lh-base">{descText}</p>
                 </div>
-              </div>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end" style={{margin: '10px'}}>
+                <button class="btn btn-primary" 
+                data-bs-toggle="modal" 
+                data-bs-target="#mainId" 
+                className="btn" 
+                type="button"
+                style={{backgroundColor: '#D9FFCF',
+                borderColor: "0E3B03",
+                boxShadow: '0 2px 5px 0 rgb(0 0 0 / 25%), 0 5px 5px 0 rgb(0 0 0 / 30%)'
+                }}
+                >Go to quiz</button>
 
-              {/*BUTTON TO STUDY TOPIC*/}
-              <div className="col-6 d-flex justify-content-center">
-                <button
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                  className="btn"
-                  style={{
-                    color: "#0e3b03",
-                    backgroundColor: "#D9FFCF",
-                    textAlign: "center",
-                    overflow: "visible",
-                    border: "2px solid",
-                    borderColor: "#0e3b03",
-                    align: "center"
-                 
-                  }}
-                >
-                  <img src="/pics/study.png" alt="study" className="zoom"
-                   
-                  />
-                </button>
-              </div>
-              {/*BUTTON FOR QUIZ*/}
-              <div className="col-6 d-flex justify-content-center">
-              <button
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                  className="btn"
-                  style={{
-                    color: "#0e3b03",
-                    backgroundColor: "#D9FFCF",
-                    textAlign: "center",
-                    overflow: "visible",
-                    border: "2px solid",
-                    borderColor: "#0e3b03",
-                    align: "center"
-                   
-                  }}
-                >
-                  <img src="/pics/quizbtn.png" alt="study" className="zoom"
-                 
-                  />
-                </button>
+                <button class="btn btn-primary" 
+                data-bs-toggle="modal" 
+                data-bs-target="#subId" 
+                className="btn" 
+                type="button"
+                style={{backgroundColor: '#0E3B03', 
+                color: 'white',
+                boxShadow: '0 2px 5px 0 rgb(0 0 0 / 25%), 0 5px 5px 0 rgb(0 0 0 / 30%)'
+                }}
+                >Download</button>
+               </div>
               </div>
             </div>
           </div>
@@ -108,7 +86,7 @@ function CourseSvn3(){
       {/* End of Topic Container */}
       {/* End of Main Layout */}
     
-          <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+      <div className="modal fade modalMain" id="mainId" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
       <div className="modal-dialog">
         <div className="modal-content" style={{ backgroundColor: '#D9FFCF' }}>
           <div className="modal-header">
@@ -126,10 +104,26 @@ function CourseSvn3(){
         </div>
       </div>
     </div>
-    
-          {/* Start of JS for Main Layout */}
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-+qU78ihVd8FhDInHkhNOl3p5nhtvZq+YP1Jv60hyVgvt/aGzfODJzL2mHTi/xIsN" crossorigin="anonymous"></script>
-          {/* End of JS for Main Layout */}
+
+    <div className="modal fade modalSub" id="subId" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+      <div className="modal-dialog">
+        <div className="modal-content" style={{ backgroundColor: '#D9FFCF' }}>
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">Take the quiz?</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body text-center">
+          <p>You will be downloading a resource file.</p><p>Do you wish to Proceed?</p>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" style={{backgroundColor: '#0e3b03', color: '#ffffff', borderRadius: '20px', fontSize: '15px', width: '100px'}}>Proceed</button>
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" style={{backgroundColor: '#0e3b03', color: '#ffffff', borderRadius: '20px', fontSize: '15px', width: '100px' }}>Cancel</button>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
         </Fragment>
       );
     }
